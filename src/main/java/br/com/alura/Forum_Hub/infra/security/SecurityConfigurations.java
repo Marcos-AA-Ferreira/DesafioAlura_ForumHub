580c2f62755185fada0e37497a41e99c0befb1bb
Marcos Antonio <marcos.56.antonio.2045@gmail.com>
2024-12-10 14:24:38 -0300

    documentacao commit

--- src/main/java/br/com/alura/Forum_Hub/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/Forum_Hub/infra/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll();
                     req.requestMatchers(HttpMethod.GET, "/forum", "/forum/{id}").permitAll();
+                    req.requestMatchers("/v3/api-docs/**",  "/swagger-ui/**", "/swagger-ui.html").permitAll();
                     req.anyRequest().authenticated();
 
                 }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
